cbbf1647c760f13825c65b1054dd8e3b5d2f2167
Pedro <pbarata97@gmail.com>
2023-06-14 01:33:34 -0300

    feat: implement method to get all news from the news API

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,22 +1,35 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import dayjs from 'dayjs'
 import ptBR from 'dayjs/locale/pt-br'
 dayjs.locale(ptBR)
 
-export function Card() {
+interface NewsProps {
+  news: {
+    title: string
+    author: string
+    description: string
+    urlToImage: string
+    publishedAt: string
+  }
+}
+
+export function Card({ news }: NewsProps) {
   return (
     <Link
-      href={`/`}
+      href={`/news/${news.title}`}
       className="w-full md:w-[300px] space-y-2 flex flex-col group"
     >
-      <div className="h-[250px] md:h-[168px] w-full md:w-[300px] relative bg-gray-100"></div>
+      <div className="h-[250px] md:h-[168px] w-full md:w-[300px] relative">
+        <Image src={news.urlToImage} alt="" fill />
+      </div>
       <div className="flex-1 flex gap-2">
         <div className="w-2 h-full bg-purple-900 rounded-full group-hover:bg-purple-700 transition-colors"></div>
         <div className="flex-1 flex flex-col justify-between gap-4">
-          <div className="font-semibold">titulo</div>
+          <div className="font-semibold">{news.title}</div>
           <div className="text-xs text-gray-500">
-            {dayjs('01/01/2000').format('DD [ de ] MMMM [ de ] YYYY')}
+            {dayjs(news.publishedAt).format('DD [ de ] MMMM [ de ] YYYY')}
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
+import axios from 'axios'
 import { Card } from './components/Card'
 import { SearchBar } from './components/SearchBar'
 
-export default function Home() {
+interface NewsContent {
+  title: string
+  author: string
+  description: string
+  urlToImage: string
+  publishedAt: string
+}
+
+interface HomeProps {
+  searchParams?: { q: string; p: number }
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const q = searchParams?.q || ''
+
+  const response = await axios.get(
+    `https://newsapi.org/v2/everything?domains=cnbc.com,ign.com&q=${q}&searchIn=title&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`,
+  )
+
+  const news: NewsContent[] = response.data.articles
+
   return (
-    <div className="space-y-5 px-5 lg:px-0 mb-10">
+    <div className="space-y-10 px-5 lg:px-0 mb-10">
       <SearchBar />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 lg:grid-cols-3">
-        <Card />
-        <Card />
-        <Card />
+        {news.map((item) => {
+          return <Card news={item} key={item.title} />
+        })}
       </div>
     </div>
   )
