568ffc2ec340feb5b624c0fbe0ec921190514f53
Pedro <pbarata97@gmail.com>
2023-06-14 01:38:39 -0300

    feat: implement search method on home page

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,13 +1,28 @@
+'use client'
+
 import { Search } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState, FormEvent } from 'react'
 
 export function SearchBar() {
+  const router = useRouter()
+
+  const [title, setTitle] = useState('')
+
+  function handlerSearchNews(event: FormEvent) {
+    event.preventDefault()
+    router.push(title === '' ? '/' : `/?q=${title}`)
+  }
+
   return (
-    <form className="flex mt-10 gap-5">
+    <form onSubmit={handlerSearchNews} className="flex mt-10 gap-5">
       <Search width={30} height={30} className="text-gray-500" />
       <input
         type="text"
         placeholder="Buscar..."
         className="focus:outline-none text-gray-500 w-full"
+        onChange={(event) => setTitle(event.target.value)}
+        onSubmit={handlerSearchNews}
       />
     </form>
   )
