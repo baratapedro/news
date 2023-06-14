395ee6f69142ffa3af535ce104f8be02dd797e1b
Pedro <pbarata97@gmail.com>
2023-06-14 01:03:14 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'assets-prd.ignimgs.com',
+      'sm.ign.com',
+      'image.cnbcfm.com',
+      'assets1.ignimgs.com',
+      'assets2.ignimgs.com',
+    ],
+  },
+}
 
 module.exports = nextConfig
